09502eb72c4629b08d18fd79baf50c35071435d9
Jonathan Mulder <mulderje@muohio.edu>
2009-07-18 17:49:32 -0500

    Changed the way the api is accesd, and changed the example a bit. Added a bit of documentation, need to do more...
    TODO: add more documentation
          add support for the provider key
          cleanup the code
    
    Signed-off-by: Jonathan Mulder <mulderje@muohio.edu>

--- example.py
+++ example.py
@@ -4,9 +4,10 @@ Example notification using prowl.
 import prowlpy
 
 apikey = '1234567890123456789012345678901234567890' #Dummy API-key)
-p = prowlpy.Prowl(apikey)
+p = prowlpy.Api(apikey)
 try:
-    p.add('TestApp','Server Down',"The Web Box isn't responding to a ping")
-    print 'Success'
+	n = prowlpy.Notification('TestApp','Server Down',"The Web Box isn't responding to a ping")
+	n.post(p.apikey)
+	print 'Success'
 except Exception,msg:
     print msg
--- prowlpy.py
+++ prowlpy.py
@@ -1,68 +1,218 @@
 # -*- coding: utf-8 -*-
-"""
-Prowlpy V0.4.1
+'''
+Prowlpy V0.5
 
 Written by Jacob Burch, 7/6/2009
+		   Jonathan Mulder, 7/18/2009
 
 Python module for posting to the iPhone Push Notification service Prowl: http://prowl.weks.net/
-"""
-__author__ = 'jacobburch@gmail.com'
-__version__ = 0.41
+'''
+__author__ = 'jacobburch@gmail.com, mulderje@muohio.edu'
+__version__ = 0.5
 
 import httplib2
 import urllib
 
 API_DOMAIN = 'https://prowl.weks.net/publicapi'
 
-class Prowl(object):
-    def __init__(self, apikey):
-        """
-        Initialize a Prowl instance.
-        """
-        self.apikey = apikey
-        
-        # Aliasing
-        self.add = self.post
-        
-    def post(self, application=None, event=None, description=None,priority=0):
-        # Create the http object
-        h = httplib2.Http()
-        
-        # Set User-Agent
-        headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
-        
-        # URL-encode and string-ify keywords. Better type/content testing is needed here
-        application = urllib.quote(str(application))
-        event = urllib.quote(str(event))
-        description = urllib.quote(str(description))
-        priority = urllib.quote(str(priority))
-        
-        # Perform the request and get the response headers and content
-        data = {
-            'apikey': self.apikey,
-            'application': application,
-            'event': event,
-            'description': description,
-            'priority': priority
+class Api(object):
+	def __init__(self,
+				 apikey=None,
+				 providerkey=None):
+		'''An object to hold a Prowler api instance
+		
+		Args:
+		   apikey: The user's API key. A 40-byte hexadecimal string.
+		   providerkey: Your provider API key. Only necessary if you have been whitelisted.
+		'''
+		self.apikey = apikey
+		self.providerkey = providerkey
 
-        }
-        headers["Content-type"] = "application/x-www-form-urlencoded"
-        resp,content = h.request("%s/add/" % API_DOMAIN, "POST", headers=headers, body=urllib.urlencode(data))
-        
-        if resp['status'] == '200':
-            return True
-        elif resp['status'] == '401': 
-            raise Exception("Auth Failed: %s" % content)
-        else:
-            raise Exception("Failed")
-        
-    
-    def verify_key(self):
-        h = httplib2.Http()
-        headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
-        verify_resp,verify_content = h.request("%s/verify?apikey=%s" % \
-                                                    (API_DOMAIN,self.apikey))
-        if verify_resp['status'] != '200':
-            raise Exception("Invalid API Key %s" % verify_content)
-        else:
-            return True
+	def get_apikey(self):
+		'''Get the api key for the prowler instance.
+		
+		Returns:
+			The apikey for the prowler instance
+		'''
+		return self._apikey
+	
+	def set_apikey(self, apikey):
+		'''Set the api key for the prowler instance.
+		
+		Args:
+		   apikey: The apikey for the prowler instance
+		'''
+		if not len(apikey) == 40:
+			raise ValueError('apikey must be 40 bytes in length')
+		self._apikey = apikey
+	
+	apikey = property(get_apikey, set_apikey,
+						doc='The api key for the prowler instance.')
+	
+	def get_providerkey(self):
+		'''Get the provider key for the prowler instance.
+		
+		Returns:
+			The providerkey for the prowler instance
+		'''
+		return self._providerkey
+	
+	def set_providerkey(self, providerkey):
+		'''Set the provider key for the prowler instance.
+		
+		Args:
+		   apikey: The apikey for the prowler instance
+		'''
+		if providerkey is not None and not len(providerkey) == 40:
+			raise ValueError('providerkey must be 40 bytes in length')
+		self._providerkey = providerkey
+	
+	providerkey = property(get_providerkey, set_providerkey,
+						doc='The provider key for the prowler instance.')
+						
+	def verify(self):
+		'''Verify an API key is valid.
+		
+		Need to add support for providerkey still...
+		'''
+		h = httplib2.Http()
+		headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
+		verify_resp,verify_content = h.request("%s/verify?apikey=%s" % \
+												(API_DOMAIN,self.apikey))
+		if verify_resp['status'] != '200':
+			raise Exception("Invalid API Key %s" % verify_content)
+		else:
+			return True
+
+class Notification(object):
+	def __init__(self,
+				 application=None,
+				 event=None,
+				 description=None,
+				 priority=0):
+		'''An object to hold a Prowler notification.
+		
+		Args:
+		   priority: An integer value ranging [-2, 2]: Very Low, Moderate, Normal, High, Emergency.
+		   application: The name of your application or the application generating the event.
+		   event: The name of the event or subject of the event.
+		   description: A description of the event, generally terse.
+		'''
+		self.priority = priority
+		self.application = application
+		self.event = event
+		self.description = description
+		
+		# Aliasing
+		self.add = self.post
+		
+		def get_priority(self):
+			'''Get the priority for the notification.
+			
+			Returns:
+				The priority for the notification
+			'''
+			return self._priority
+			
+		def set_priority(self, priority):
+			'''Sets the priority for the notification.
+			
+			Args:
+				priority: The priority for the notification
+			'''
+			if not (priority >= -2 or priority <= 2):
+				raise ValueError('priority must be in the range [-2, 2]')
+			self._priority = prioriy
+			
+		priority = property(get_priority, set_priority,
+							doc='The priority for the notification')
+							
+		def get_event(self):
+			'''Get the event for the notification
+			
+			Returns:
+				The event for the notification
+			'''
+			return self._event
+			
+		def set_event(self, event):
+			'''Sets the event for the notification
+			
+			Args:
+				event: The event for the notification
+			'''
+			if not len(event) <= 1024:
+				raise ValueError('events length must be <= 1024')
+			self._event = unicode(event)
+			
+		event = property(get_event, set_event,
+								doc='The event for the notification')
+								
+		def get_event(self):
+			'''Get the event for the notification
+
+			Returns:
+				The event for the notification
+			'''
+			return self._event
+
+		def set_event(self, event):
+			'''Sets the event for the notification
+
+			Args:
+				event: The event for the notification
+			'''
+			if not len(event) <= 1024:
+				raise ValueError('events length must be <= 1024')
+			self._event = unicode(event)
+
+		event = property(get_event, set_event,
+								doc='The event for the notification')
+								
+		def get_description(self):
+			'''Get the description for the notification
+
+			Returns:
+				The description for the notification
+			'''
+			return self._description
+
+		def set_description(self, description):
+			'''Sets the description for the notification
+
+			Args:
+				description: The description for the notification
+			'''
+			if not len(description) <= 10000:
+				raise ValueError('description length must be <= 10000')
+			self._description = unicode(description)
+
+		description = property(get_description, set_description,
+								doc='The description for the notification')
+
+	def post(self,
+			 apikey=None,
+			 providerkey=None):
+        # Create the http object
+		h = httplib2.Http()
+
+        # Set User-Agent
+		headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
+
+        # Perform the request and get the response headers and content
+		data = {
+			'apikey': apikey,
+			'application': self.application,
+			'event': self.event,
+			'description': self.description,
+			'priority': self.priority
+		}
+		headers["Content-type"] = 'application/x-www-form-urlencoded'
+		resp,content = h.request("%s/add/" % API_DOMAIN, "POST", headers=headers, body=urllib.urlencode(data))
+
+		if resp['status'] == '200':
+			return True
+		elif resp['status'] == '401':
+			raise Exception("Auth Failed: %s" % content)
+		else:
+			raise Exception('Failed')
