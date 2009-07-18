17490514e9dbbd099d8d5ae446a9b77c3da44dc3
Jonathan Mulder <mulderje@muohio.edu>
2009-07-18 18:59:32 -0500

    Added MIT licence, and changed the way the version is shown
    Signed-off-by: Jonathan Mulder <mulderje@muohio.edu>

--- prowlpy.py
+++ prowlpy.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2009 Jonathan Mulder <mulderje@muohio.edu>
+# 					 Jacob Burch <jacobburch@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 '''
 Prowlpy v0.5.2
 
@@ -8,7 +29,7 @@ Written by Jacob Burch, 7/6/2009
 Python module for posting to the iPhone Push Notification service Prowl: http://prowl.weks.net/
 '''
 __author__ = 'jacobburch@gmail.com, mulderje@muohio.edu'
-__version__ = 0.52
+__version__ = '0.5.2'
 
 import httplib2
 import urllib
